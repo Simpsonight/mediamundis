ce8f564e4755c819289f598a72f694b7912663d2
Simon Kemmerling <simon.kemmerling@kmh-gmbh.com>
2022-08-21 21:59:25 +0200

    intro

---
+++ app.vue
@@ -0,0 +1,35 @@
+<script setup>
+useHead({
+  title: 'Simon Kemmerling',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [
+    { name: 'description', content: 'Simon Kemmerling. Full Stack Developer and Consultant.' }
+  ],
+  bodyAttrs: {
+    class: 'app-view'
+  }
+})
+</script>
+
+<template>
+  <div id="app">
+    <Intro />
+  </div>
+</template>
+
+<style>
+.app-view {
+  background: #fff url(/img/muster_bg4.gif) top left;
+}
+
+img {max-width: 100%;}
+
+#app {
+  height: 160px;
+  background: #fff;
+  text-align: left;
+  padding: 140px 20px 0 20px;
+}
+
+</style>
---
+++ components/Intro.vue
@@ -0,0 +1,23 @@
+<template>
+    <img src="/img/mediamundis.png" width="397" height="54" alt="mediamundis - Simon Kemmerling" />
+    <h1 class="font-sm pb-0">Simon Kemmerling <br><span class="grey">Full Stack Developer and Consultant</span></h1>
+    <p class="font-sm pt-0 grey"><a href="mailto:sk@mediamundis.de">sk@mediamundis.de</a> | <a href="https://www.linkedin.com/in/simon-kemmerling/">LinkedIn</a> | <a href="https://www.xing.com/profile/Simon_Kemmerling/cv">Xing</a></p>
+</template>
+
+<style scoped>
+    .font-sm {
+        font: 12px Arial, Helvetica, sans-serif;
+        padding: 14px;
+    }
+
+    .pt-0 {padding-top:0}
+    .pb-0 {padding-bottom: 0;}
+
+    .grey {color: #999999;}
+
+    a { text-decoration: none; }
+    a:link { color: #999999; }
+    a:visited { color: #999999; }
+    a:hover, a:focus { color: #ff6633; }
+    a:active { color: #999999; }
+</style>
